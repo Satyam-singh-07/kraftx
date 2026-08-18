048785389fc219ce2f20465e35a566451d0a3413
satyam <satyamsingh962572@gmail.com>
2026-08-18 11:33:19 +0530

    add the new mail no-reply.thekraftx.com

--- app/Http/Controllers/Public/BulkOrderInquiryController.php
+++ app/Http/Controllers/Public/BulkOrderInquiryController.php
@@ -9,6 +9,7 @@ use App\Mail\BulkOrderCustomerConfirmationMail;
 use App\Models\BulkOrderInquiry;
 use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
